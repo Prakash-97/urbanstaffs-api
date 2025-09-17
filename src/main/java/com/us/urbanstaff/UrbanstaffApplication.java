078539bc61e53c6e0620852e6906304f2ddde40f
Ravi Singh <kr.prakash37@gmail.com>
2025-09-17 11:55:46 +0530

    adding my sql dependencies

---
+++ src/main/java/com/us/urbanstaff/UrbanstaffApplication.java
@@ -0,0 +1,14 @@
+package com.us.urbanstaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanstaffApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UrbanstaffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/us/urbanstaff/UrbanstaffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.us.urbanstaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanstaffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
